6010adc9550b142c0a483eb722fd43a54712b08f
Nikolay <krukolnik@gmail.com>
2022-11-19 17:25:45 +0300

    solution for task_6

--- chapter_4/task_6.py
+++ chapter_4/task_6.py
@@ -30,36 +30,48 @@ class MaxHeap:
 
     def _siftUp(self):
         i_ne = len(self._heap) - 1
-        i_p = self._getParentIndex()
+        i_p = self._getParentIndex(i_ne)
+        if i_p == i_ne or i_p == -1 or i_ne == -1:
+            return
         while self._heap[i_p] < self._heap[i_ne]:
             self._swapValuePosition(i_ne, i_p)
             i_ne = i_p
-            i_p = self._getParentIndex()
+            i_p = self._getParentIndex(i_ne)
 
     def _swapValuePosition(self, indx1, indx2):
         self._heap[indx1], self._heap[indx2] = self._heap[indx2], self._heap[indx1]
 
 
     def _getParentIndex(self, index):
-        return index//self._numberChildNode
+        if self._numberChildNode:
+            pi = (index + 1) // self._numberChildNode - 1
+            if pi < 0:
+                pi = 0
+            return pi
+        else:
+            return -1
+
 
     def _getIndexChildWithMaxValue(self, index_parent):
-        index_child1 = index_parent * self._numberChildNode
-        index_child2 = index_parent * self._numberChildNode + 1
+        index_child1 = (index_parent + 1) * self._numberChildNode - 1
+        index_child2 = (index_parent + 1) * self._numberChildNode
         value_child1 = -1
         value_child2 = -1
-        if index_child1 in self._heap:
+
+        if index_child1 < len(self._heap):
             value_child1 = self._heap[index_child1]
-            if value_child2 in self._heap:
-                value_child2 = self._heap[index_child2]
-                if value_child1 > value_child2:
-                    return  True, index_child1
-                else:
-                    return True, index_child2
-            else:
-                return True, index_child1
-        else:
+        if index_child2 < len(self._heap):
+            value_child2 = self._heap[index_child2]
+
+        if value_child1 == -1 and value_child2 == -1:
             return False, -1
+
+        if value_child1 >= value_child2:
+            return  True, index_child1
+        else:
+            return True, index_child2
+
+
 
 
 n = int(input())
