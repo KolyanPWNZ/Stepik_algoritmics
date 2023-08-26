4b24fd840b0d12e985b8fe5e55e472c0e465b0b3
Nikolay <krukolnik@gmail.com>
2023-08-26 13:39:07 +0300

    the first solution

--- chapter_8/task_2.py
+++ chapter_8/task_2.py
@@ -1,5 +1,32 @@
 import sys
 
+# 5
+# 5 3 4 4 2
+
+
+def lds_bu(n: int, seq: list) -> tuple:
+    d = list()
+    prev = list()
+    for i in range(n):
+        d.append(1)
+        prev.append(-1)
+        for j in range(i):
+            if seq[i] <= seq[j] and d[j] + 1 > d[i]:
+                d[i] = d[j] + 1
+                prev[i] = j
+
+    ans = max(d)
+    k = d.index(ans)
+    lds_indexes = [0 for _ in range(ans)]
+
+    j = ans-1
+    while k >= 0:
+        lds_indexes[j] = k + 1
+        j = j-1
+        k = prev[k]
+
+    return ans, lds_indexes
+
 
 def read_input():
     n = int(sys.stdin.readline())
@@ -8,7 +35,9 @@ def read_input():
 
 
 def main():
-    n, seq = read_input()
+    k, indexes = lds_bu(*read_input())
+    print(k)
+    print(*indexes)
 
 
 if __name__ == "__main__":
