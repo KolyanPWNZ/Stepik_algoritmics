c8c66e0c1f695dac42c062c25c1db4b863faaca2
Nikolay <krukolnik@gmail.com>
2023-08-27 18:10:47 +0300

    the task is solved + added testing

--- chapter_8/task_2.py
+++ chapter_8/task_2.py
@@ -1,10 +1,11 @@
 import sys
 
-def binary_search(tails, key):
-    left, right = 0, len(tails) - 1
+
+def binary_search(tails, key, length):
+    left, right = 0, length - 1
     while left <= right:
         mid = left + (right - left) // 2
-        if tails[mid] > key:
+        if tails[mid] >= key:
             left = mid + 1
         else:
             right = mid - 1
@@ -13,27 +14,32 @@ def binary_search(tails, key):
 
 # trying to make an algorithm O(nlog(n))
 def lds_bu_faster(n: int, arr: list):
-    n = len(arr)
     tails = [0] * n  # Массив для отслеживания "хвостов" последовательностей
-    lds_indexes = [-1] * n  # Массив индексов ННП (наиб. невоз. последов.)
     tails[0] = arr[0]
-    lds_indexes[0] = 1
+    lds_indexes = [0] * n  # Массив для индексов предыдущих элементов
     length = 1
 
     for i in range(1, n):
-        if arr[i] >= tails[0]:
+        if arr[i] > tails[0]:
             tails[0] = arr[i]
-            lds_indexes[0] = i + 1
+            lds_indexes[i] = 0
         elif arr[i] <= tails[length - 1]:
             tails[length] = arr[i]
-            lds_indexes[length] = i + 1
+            lds_indexes[i] = length
             length += 1
         else:
-            pos = binary_search(tails[:length], arr[i])
+            pos = binary_search(tails, arr[i], length)
             tails[pos] = arr[i]
-            lds_indexes[pos] = i + 1
+            lds_indexes[i] = pos
 
-    return length, lds_indexes[:length]
+    lds_sequence_indexes = [-1] * length
+    current_length = length - 1
+    for i in range(n - 1, -1, -1):
+        if lds_indexes[i] == current_length:
+            lds_sequence_indexes[current_length] = i + 1
+            current_length -= 1
+
+    return length, lds_sequence_indexes
 
 
 # O(n**2)
@@ -74,5 +80,24 @@ def main():
     print(*indexes)
 
 
+def test():
+    assert lds_bu_faster(5, [5, 3, 4, 4, 2]) == (4, [1, 3, 4, 5])                               # test 1
+    assert lds_bu_faster(1, [1]) == (1, [1])                                                    # test 2
+    assert lds_bu_faster(2, [1, 2]) == (1, [2])                                                 # test 3
+    assert lds_bu_faster(2, [2, 1]) == (2, [1, 2])                                              # test 4
+    assert lds_bu_faster(2, [1, 1]) == (2, [1, 2])                                              # test 5
+    assert lds_bu_faster(10, [9, 10, 6, 3, 6, 8, 7, 9, 6, 5]) == (5, [2, 6, 7, 9, 10])          # test 6
+    assert lds_bu_faster(10, [2, 3, 1, 2, 1, 3, 2, 1, 1, 2]) == (5, [2, 6, 7, 8, 9])             # test 7
+    assert lds_bu_faster(100, [64, 80, 90, 69, 23, 78, 30, 76, 11, 2, 7, 42, 13, 45,
+                               40, 52, 31, 42, 33, 74, 37, 64, 2, 26, 88, 51, 53, 95,
+                               26, 7, 58, 9, 30, 29, 53, 69, 3, 71, 40, 89, 32, 90,
+                               14, 49, 18, 70, 37, 79, 6, 48, 25, 28, 7, 81, 35, 99,
+                               1, 1, 28, 17, 56, 77, 11, 64, 28, 6, 9, 62, 32, 2, 31,
+                               29, 23, 50, 89, 95, 54, 36, 13, 52, 77, 58, 80, 58, 21,
+                               20, 1, 82, 27, 31, 84, 14, 73, 55, 2, 69, 95, 33, 70, 72]) \
+           == (18, [3, 6, 8, 20, 22, 31, 35, 44, 50, 55, 69, 71, 72, 73, 85, 86, 92, 95])       # test 8
+
+
 if __name__ == "__main__":
+    # test()
     main()
