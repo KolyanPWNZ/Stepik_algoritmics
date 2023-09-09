850821be8de51176ab450c7a94bd496bfc1189ac
Nikolay <krukolnik@gmail.com>
2023-09-09 16:28:16 +0300

    added a solution for tast 4

--- chapter_8/task_4.py
+++ chapter_8/task_4.py
@@ -1,8 +1,16 @@
 import sys
 
 
-def knapsack_bu(capacity: int, n: int, w: list) -> int:
-    return 1
+def knapsack_bu(capacity: int, n: int, W: list) -> int:
+    d = [[0] * (capacity+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        for w in range(1, capacity+1):
+            d[i][w] = d[i-1][w]
+            if W[i-1] <= w:
+                d[i][w] = max(d[i][w],
+                               d[i-1][w-W[i-1]] + W[i-1])
+    return d[n][capacity]
 
 
 def read_data():
@@ -12,7 +20,7 @@ def read_data():
 
 
 def main():
-    knapsack_bu(*read_data())
+    print(knapsack_bu(*read_data()))
 
 
 if __name__ == "__main__":
